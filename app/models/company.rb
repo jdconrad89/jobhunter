c8807c9165c52e1bda357c4deef9b5174e2ee52d
Jason Conrad <jason.d.conrad.89@gmail.com>
2026-05-06 19:39:18 -0400

    add TODO to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,7 @@ class Company < ApplicationRecord
   has_many :job_posts, dependent: :destroy
 
   validates :name, presence: true
+
+
+  # TODO: Add logic to manually add information about a company and also view all JobPosts/JobApplications for a company
 end
